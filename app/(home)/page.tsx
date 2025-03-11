104add9c9f84d12c3baec3f267b207ab611f3313
Candra Wali Sanjaya <82704254+chndrwali@users.noreply.github.com>
2025-03-11 19:02:44 +0700

    (add): trpc

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <>I Will</>;
+import { HydrateClient, trpc } from '@/trpc/server';
+import { ErrorBoundary } from 'react-error-boundary';
+import { Suspense } from 'react';
+import { PageClient } from './client';
+
+export default async function Home() {
+  void trpc.hello.prefetch({ text: 'goblok' });
+
+  return (
+    <HydrateClient>
+      <Suspense fallback={<p>Loading...</p>}>
+        <ErrorBoundary fallback={<p>Error...</p>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
+  );
 }
