a80e2b6d7ff4ac1aff505fbd410e522f7932d338
Candra Wali Sanjaya <82704254+chndrwali@users.noreply.github.com>
2025-03-14 15:10:51 +0700

    add: video reactions

--- modules/videos/ui/components/video-reactions.tsx
+++ modules/videos/ui/components/video-reactions.tsx
@@ -2,19 +2,55 @@ import { cn } from '@/lib/utils';
 import { ThumbsUpIcon, ThumbsDownIcon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
+import { VideoGetOneOutput } from '../../types';
+import { useClerk } from '@clerk/nextjs';
+import { trpc } from '@/trpc/client';
+import { toast } from 'sonner';
 
-export const VideoReactions = () => {
-  const viewerReaction: 'like' | 'dislike' = 'like';
+interface VideoReactionsProps {
+  videoId: string;
+  likes: number;
+  dislikes: number;
+  viewerReaction: VideoGetOneOutput['viewerReaction'];
+}
+
+export const VideoReactions = ({ videoId, dislikes, likes, viewerReaction }: VideoReactionsProps) => {
+  const clerk = useClerk();
+  const utils = trpc.useUtils();
+
+  const like = trpc.videoReactions.like.useMutation({
+    onSuccess: () => {
+      utils.videos.getOne.invalidate({ id: videoId });
+    },
+    onError: (error) => {
+      toast.error('Ada sesuatu yang salah!');
+      if (error.data?.code === 'UNAUTHORIZED') {
+        clerk.openSignIn();
+      }
+    },
+  });
+  const dislike = trpc.videoReactions.dislike.useMutation({
+    onSuccess: () => {
+      utils.videos.getOne.invalidate({ id: videoId });
+    },
+    onError: (error) => {
+      toast.error('Ada sesuatu yang salah!');
+      if (error.data?.code === 'UNAUTHORIZED') {
+        clerk.openSignIn();
+      }
+    },
+  });
+
   return (
     <div className="flex items-center flex-none">
-      <Button variant="secondary" className="rounded-l-full rounded-r-none gap-2 pr-4">
-        <ThumbsUpIcon className={cn('size-5', viewerReaction === 'like' && 'fill-black')} />
-        {1}
+      <Button onClick={() => like.mutate({ videoId })} disabled={like.isPending || dislike.isPending} variant="secondary" className="rounded-l-full rounded-r-none gap-2 pr-4">
+        <ThumbsUpIcon className={cn('size-5', viewerReaction === 'like' && 'fill-black text-blue-500')} />
+        {likes}
       </Button>
       <Separator orientation="vertical" className="h-7" />
-      <Button variant="secondary" className="rounded-r-full rounded-l-none pl-3">
-        <ThumbsDownIcon className={cn('size-5', viewerReaction !== 'like' && 'fill-black')} />
-        {1}
+      <Button onClick={() => dislike.mutate({ videoId })} disabled={like.isPending || dislike.isPending} variant="secondary" className="rounded-r-full rounded-l-none pl-3">
+        <ThumbsDownIcon className={cn('size-5', viewerReaction === 'dislike' && 'fill-black')} />
+        {dislikes}
       </Button>
     </div>
   );
--- trpc/routers/_app.ts
+++ trpc/routers/_app.ts
@@ -4,12 +4,14 @@ import { videoRouter } from '@/modules/videos/server/procedures';
 import { studioRouter } from '@/modules/studio/server/procedures';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
 import { videoViewsRouter } from '@/modules/video-views/server/procedures';
+import { videoReactionsRouter } from '@/modules/video-reactions/server/procedures';
 
 export const appRouter = createTRPCRouter({
   categories: categoriesRouter,
   studio: studioRouter,
   videos: videoRouter,
   videoViews: videoViewsRouter,
+  videoReactions: videoReactionsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
