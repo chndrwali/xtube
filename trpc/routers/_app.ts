de3fc36de94401d8e788ce9eb84dfcfa7944017a
Candra Wali Sanjaya <82704254+chndrwali@users.noreply.github.com>
2025-03-15 21:38:51 +0700

    add: suggestions

--- trpc/routers/_app.ts
+++ trpc/routers/_app.ts
@@ -5,14 +5,16 @@ import { studioRouter } from '@/modules/studio/server/procedures';
 import { commentsRouter } from '@/modules/comments/server/procedures';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
 import { videoViewsRouter } from '@/modules/video-views/server/procedures';
-import { videoReactionsRouter } from '@/modules/video-reactions/server/procedures';
+import { suggestionsRouter } from '@/modules/suggestions/server/procedures';
 import { subscriptionsRouter } from '@/modules/subscriptions/server/procedures';
+import { videoReactionsRouter } from '@/modules/video-reactions/server/procedures';
 import { commentReactionsRouter } from '@/modules/comment-reactions/server/procedures';
 
 export const appRouter = createTRPCRouter({
   categories: categoriesRouter,
   studio: studioRouter,
   videos: videoRouter,
+  suggestions: suggestionsRouter,
   videoViews: videoViewsRouter,
   videoReactions: videoReactionsRouter,
   subscriptions: subscriptionsRouter,
